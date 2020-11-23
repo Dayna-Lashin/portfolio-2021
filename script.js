d567fafc5feb00c97d5dae86cdbd8d820d46f79e
dayna_l <dayna.lashin@gmail.com>
2020-11-22 23:56:40 -0500

    Added comments

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var colors = ["#FF6B6B","#FFE66D","#4472CA"]; // array for the color of the shap
 function createParticle(event){
     var x = event.clientX; // x coordinate of mouse click
     var y = event.clientY; // y coordinate of mouse click
-    var color = Math.floor(Math.random() * 3); // random number 0-3 rounded down to nearest whole integer (3 because there are 3 colors in array)
+    var color = Math.floor(Math.random() * 3); // sets color to random number 0-3 rounded down to nearest whole integer
 
     var div = document.createElement("div"); // create a new div element
     div.style.position = "absolute"; // gives div a position of absolute
@@ -15,7 +15,7 @@ function createParticle(event){
     div.style.borderTop = "5px solid transparent"; // gives div a top border that's 5px, solid, and transparent
     div.style.borderRight = "5px solid transparent"; // gives div a right border that's 5px, solid, and transparent
     div.style.borderLeft = "5px solid transparent"; // gives div a left border that's 5px, solid, and transparent
-    div.style.borderBottom = "10px solid " + colors[color]; // gives div a bottom border that's 10px, solid, and a color that corresponds with the random number in the color array
+    div.style.borderBottom = "10px solid " + colors[color]; // gives div a bottom border that's 10px, solid, and a color that corresponds with the value of color var
     div.style.animation = "move 10s ease-in infinite"; // goes with keyframes for move and sets animation to 10 second duration, ease-in for timimg and infinite for animation iteration
     particles.appendChild(div); // appends new div to partivle div
 
@@ -28,14 +28,14 @@ function createParticle(event){
 }
 
 function getParticles(){
-    var np = document.documentElement.clientWidth / 40; // divides viewable HTML element width by 40 (viewable includes padding only)
-    particles.innerHTML = ""; // returns particle HTML content as empty string
-    for (var i = 0; i < np; i++) { // FOR LOOP starting at 0 increasing by 1 for as long as it's less than variable np run the below
+    var np = document.documentElement.clientWidth / 40; // divides viewable HTML element width by 40 which affects how many particles there are
+    particles.innerHTML = ""; // sets particle HTML content as empty string
+    for (var i = 0; i < np; i++) { // FOR LOOP starting at 0 increasing by 1 for as long as i is less than the number of new particles run the below
         var w = document.documentElement.clientWidth; // get viewable HTML element width set to w
         var h = document.documentElement.clientHeight; // get viewable HTML element height set to h
-        var rndw = Math.floor(Math.random() * w ) + 1; // random number bewteen 0 - viewable width  - rounded down to nearest whole integer plus 1
-        var rndh = Math.floor(Math.random() * h ) + 1; // random number bewteen 0 - viewable height  - rounded down to nearest whole integer +1
-        var widthpt = Math.floor(Math.random() * 8) + 5; // random number between 0-8 rounded down to nearest whole integer +5
+        var rndw = Math.floor(Math.random() * w ) + 1; // random number bewteen 0 - viewable width rounded down to nearest whole integer plus 1 - where appears on screen x axis
+        var rndh = Math.floor(Math.random() * h ) + 1; // random number bewteen 0 - viewable height  - rounded down to nearest whole integer +1 - where appears on screen y axis
+        var widthpt = Math.floor(Math.random() * 8) + 5; // random number between 0-8 rounded down to nearest whole integer +5 - width of particle
         var opty = Math.floor(Math.random() * 4) + 1; // random number between 0-4 rounded down to nearest whole integer +1
         var anima = Math.floor(Math.random() * 12) + 8; // random number between 0-12 rounded down to nearest whole integer +8
         var bdr = Math.floor(Math.random() * 2); // random number between 0-2 rounded down to nearest whole integer (2 because there are 2 border options in array)
